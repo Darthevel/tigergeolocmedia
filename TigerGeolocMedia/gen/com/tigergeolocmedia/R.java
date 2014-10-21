749d23c4154cff4b2c24376a0f82a8adb3ab4f83
Darthevel <frederic.darthevel@gmail.com>
2014-10-21 15:14:26 +0200

    Evolution #61 : Développement Caméra.

--- TigerGeolocMedia/gen/com/tigergeolocmedia/R.java
+++ TigerGeolocMedia/gen/com/tigergeolocmedia/R.java
@@ -25,7 +25,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int buttonMovie=0x7f080003;
+        public static final int buttonPicture=0x7f080001;
+        public static final int buttonSound=0x7f080002;
+        public static final int linearLayout1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,7 +41,9 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
-        public static final int welcome_message=0x7f050003;
+        public static final int takeMovie=0x7f050004;
+        public static final int takePicture=0x7f050003;
+        public static final int takeSound=0x7f050005;
     }
     public static final class style {
         /** 
--- TigerGeolocMedia/src/com/tigergeolocmedia/MainActivity.java
+++ TigerGeolocMedia/src/com/tigergeolocmedia/MainActivity.java
@@ -1,16 +1,127 @@
 package com.tigergeolocmedia;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	public final static String PICTURE_DIRECTORY = "/DCIM/Camera/TigerCamera";
+	public final static String MOVIE_DIRECTORY = "DCIM/Camera/TigerCamera";
+	public final static String IMAGE_PREFIX = "TIGER_";
+	public static final String JPEG_FILE_SUFFIX = ".jpg";
+	private static final int ACTION_TAKE_PHOTO_B = 1;
+
+	public final static String IMAGE_SUFFIX = JPEG_FILE_SUFFIX;
+	private Button buttonPicture;
+	private Button buttonMovie;
+	private Button buttonSound;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		buttonPicture = (Button) findViewById(R.id.buttonPicture);
+		buttonPicture.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				takePicture();
+			}
+			
+		});
+			
+		buttonMovie = (Button) findViewById(R.id.buttonMovie);
+		buttonMovie.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				takeMovie();
+			}
+			
+		});
+		buttonSound = (Button) findViewById(R.id.buttonSound);
+		buttonSound.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				taKeSound();
+			}
+			
+		});
+		
+	}
+
+	protected void taKeSound() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void takeMovie() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void takePicture() {
+		// Création du fichier où la photo sera sauvegardée.
+		File pictureFile = null;
+		try {
+			pictureFile = createPictureFile();
+			if (pictureFile == null) {
+				return;
+			}
+			Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			
+			takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(pictureFile));
+			
+			startActivityForResult(takePictureIntent, ACTION_TAKE_PHOTO_B);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+
+		
+		
+	}
+
+	@SuppressLint("SimpleDateFormat")
+	private File createPictureFile() throws IOException {
+		
+		
+		if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+			// Todo : afficher un dialog
+			return null;
+		}
+		
+		String mediaMounted = Environment.MEDIA_MOUNTED;
+		String externalStorageState = Environment.getExternalStorageState();
+		
+		File externalStorageDirectory  = Environment.getExternalStorageDirectory();
+
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String imageFileName = IMAGE_PREFIX + timeStamp + "_";
+		File pictureDirectory = new File(externalStorageDirectory + "/" + PICTURE_DIRECTORY);
+		boolean exists = pictureDirectory.exists();
+		if (!exists) {
+			pictureDirectory.mkdirs();
+		}
+		File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, pictureDirectory);
+		return imageF;
 	}
 
 	@Override
