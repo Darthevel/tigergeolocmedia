061b2ed04a43c6784fc6e4b7c68be2c162a74999
Gael <thiebaut.gael@gmail.com>
2014-10-22 14:37:10 +0200

    Ajout de la classe Media

---
+++ TigerGeolocMedia/src/com/tigergeolocmedia/Media.java
@@ -0,0 +1,15 @@
+package com.tigergeolocmedia;
+
+enum MediaType {
+	PICTURE,
+	MOVIE,
+	SOUND
+};
+
+public class Media {
+	
+	private MediaType type;
+	private String name;
+	private String path;
+
+}
