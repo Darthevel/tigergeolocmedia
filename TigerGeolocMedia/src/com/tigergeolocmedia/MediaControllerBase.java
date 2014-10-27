3d8f7c960a8b2f4f025f47279b9dfa13af3506b2
Darthevel <frederic.darthevel@gmail.com>
2014-10-27 14:38:35 +0100

    ajout de Mediacontroller.java et MovieController.java

---
+++ TigerGeolocMedia/src/com/tigergeolocmedia/MediaControllerBase.java
@@ -0,0 +1,82 @@
+package com.tigergeolocmedia;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.os.Environment;
+
+public abstract class MediaControllerBase implements MediaController { 
+	
+	protected String prefix;
+	protected String suffix;
+	protected String directory;
+
+	protected Media media;
+	
+	
+	
+	public MediaControllerBase(String prefix, String suffix, String directory) {
+		super();
+		this.prefix = prefix;
+		this.suffix = suffix;
+		this.directory = directory;
+	}
+
+
+
+	protected File createFile() throws IOException {
+		if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+			// Todo : afficher un dialog ou lever une exception
+			return null;
+		}
+		
+		
+		File externalStorageDirectory  = Environment.getExternalStorageDirectory();
+
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat(Constants.TIME_STAMP_FORMAT).format(new Date());
+		String imageFileName = prefix + timeStamp + "_";
+		File mediaDirectory = new File(externalStorageDirectory + "/" + directory);
+		boolean exists = mediaDirectory.exists();
+		if (!exists) {
+			mediaDirectory.mkdirs();
+		}
+		File imageF = File.createTempFile(imageFileName, suffix, mediaDirectory);
+		return imageF;
+	}
+
+
+
+	public String getPrefix() {
+		return prefix;
+	}
+
+
+
+	public String getSuffix() {
+		return suffix;
+	}
+
+
+
+	public String getDirectory() {
+		return directory;
+	}
+
+
+
+	public Media getMedia() {
+		return media;
+	}
+
+
+
+	public void setMedia(Media media) {
+		this.media = media;
+	}
+	
+	
+
+}
---
+++ TigerGeolocMedia/src/com/tigergeolocmedia/MovieController.java
@@ -0,0 +1,59 @@
+package com.tigergeolocmedia;
+
+import java.io.File;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public class MovieController extends MediaControllerBase {
+	private Activity activity;
+	
+	
+
+	public MovieController(String prefix, String suffix, String directory, Activity activity) {
+		super(prefix, suffix, directory);
+		this.activity = activity;
+	}
+
+	@Override
+	public void record() {
+		// Cr�ation du fichier o� la photo sera sauvegard�e.
+		File file = null;
+		try {
+			// file = createMovieFile();
+			file = createFile();
+			if (file == null) {
+				return;
+			}
+			Media media = new Media(MediaType.MOVIE);
+			this.media = media;
+			media.setPath(file.getAbsolutePath());
+			media.setName(file.getName());
+			Intent takePictureIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+			
+			takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+			
+			activity.startActivityForResult(takePictureIntent, Constants.ACTION_TAKE_MOVIE);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
