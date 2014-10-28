1cac70ceeff7861ce4eb998d3ae32ad8cc9f1c5f
Michael Adjedj <spcj.michael@gmail.com>
2014-10-28 14:37:38 +0100

    Ajout du tiroir de type Drawer

--- TigerGeolocMedia/src/com/tigergeolocmedia/MainActivity.java
+++ TigerGeolocMedia/src/com/tigergeolocmedia/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ParentMenuActivity {
 
 	private Button buttonPicture;
 	private Button buttonMovie;
