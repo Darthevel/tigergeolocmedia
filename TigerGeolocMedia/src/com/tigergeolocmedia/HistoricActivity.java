b99042aefcb25cd8069ff07ea592d16b317cda63
Gael <thiebaut.gael@gmail.com>
2014-10-31 11:50:33 +0100

    Ajout d'un feedback quand on click sur un item dans l'historic (utilisation de RXJava)

--- TigerGeolocMedia/src/com/tigergeolocmedia/HistoricActivity.java
+++ TigerGeolocMedia/src/com/tigergeolocmedia/HistoricActivity.java
@@ -1,23 +1,30 @@
 package com.tigergeolocmedia;
 
 import rx.Observable;
+import rx.android.events.OnItemClickEvent;
+import rx.android.observables.ViewObservable;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class HistoricActivity extends ParentMenuActivity {
 
 	private ListView listView;
 	private Historic historic;
+	private Context context;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_historic);
 		
-		historic = Historic.getInstance(getApplicationContext());
-		ListElementFactory factory = new ListElementFactory(getApplicationContext());
+		context = getApplicationContext();
+		historic = Historic.getInstance(context);
+		ListElementFactory factory = new ListElementFactory(context);
 		Observable<HistoricElement> observable = Observable.create(factory);
 		observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
 		CustomListAdapter adapter = new CustomListAdapter(this, observable);
@@ -26,26 +33,16 @@ public class HistoricActivity extends ParentMenuActivity {
 		this.listView = (ListView) findViewById(R.id.listView);
 
 		listView.setAdapter(adapter);
-		
-			
-		
-//			this.listView.setOnItemClickListener(new OnItemClickListener() {
-//				@Override
-//				public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
-//				{
-//					// TODO Auto-generated method stub
-//					HashMap<String, String> element = (HashMap<String, String>) parent.getItemAtPosition(position);
-//					Toast.makeText(getApplicationContext(), "Click ListItem Number " + (position + 1) + "\n" + element.get("nom") + " " + element.get("tel"), Toast.LENGTH_SHORT).show();
-//				  	Intent intent = new Intent(getApplicationContext(), ContactDescriptionActivity.class);
-//				  	intent.putExtra("element", element);
-//				  	startActivity(intent);
-//				}
-//			});
-	        
-		
-		
-	        //ListAdapter adapter = new SimpleAdapter(this, list, R.layout.listviewitem, new String[] {"nom", "tel"}, new int[] {R.id.itemTitle, R.id.itemDesc});
-	        //this.listView.setAdapter(adapter);
-		
+		ViewObservable.itemClicks(listView)
+			.subscribe(new Action1<OnItemClickEvent>(){
+
+				@Override
+				public void call(OnItemClickEvent e) {
+					int position = listView.getPositionForView(e.view);
+					Media media = historic.getMediaList().get(position);
+					Toast.makeText(context, "Vous avez clique sur" + media.getName(), Toast.LENGTH_SHORT).show();
+				}
+				
+			});
 	}
 }
